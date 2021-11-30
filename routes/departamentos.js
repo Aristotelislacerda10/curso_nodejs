d53f2ed0bcdeeb9707f2a13373089269087834e7
Aristoteles Lacerda <aristotelislacerda10@gmail.com>
2021-11-30 14:00:43 -0300

    fazendo upload de imagem

--- routes/departamentos.js
+++ routes/departamentos.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        let data = new Date().toISOString().replace(/:/g, '-') + '-';
+        cb(null, data +  file.originalname);  
+    }
+})
+
+const upload = multer({
+    storage : storage,
+    limits:{
+        fileSeze: 1024 * 2014 * 5
+    }
+})
+
+
 
 // retorna departamentos
 router.get('/', (req, res, next) => {
@@ -13,15 +33,24 @@ mysql.getConnection((error, conn) => {
         'select * from departamento',
         (error, resultado, fields) => {
             if (error) { return res.status(500).send({error : error})}  
-            return res.status(200).send({Response : resultado}) 
+            const response = {
+                quantidade: resultado.length,
+                departamento: resultado.map(dep =>{
+                    return{
+                      id: dep.id,
+                      descricao_dep: dep.descricao_dep
+                    }  
+                })
+             } 
+            return res.status(200).send(response) 
         }
     )
 })
 
 });
 
-router.post('/', (req, res, next) => {
-
+router.post('/', upload.single('dep_imagem'), (req, res, next) => {
+    console.log(req.file);
     const departamento = {
         descricao_dep: req.body.nome
     };
@@ -54,7 +83,7 @@ router.get('/:id_dep', (req, res, next) => {
             'select * from departamento where id = ?',
             [req.params.id_dep],
             (error, resultado, fields) => {
-                if (error) { return res.status(500).send({error : error})}  
+                if (error) { return res.status(500).send({error : error})} 
                 return res.status(200).send({Response : resultado}) 
             }
         )
@@ -74,9 +103,8 @@ router.patch('/', (req, res, next) => {
               conn.release();  
               if (error) { return res.status(500).send({error : error})} 
               
-              res.status(201).send({
-               mensagem: 'departamento alterado com sucesso',
-               id: resultado.insertId
+              res.status(202).send({
+               mensagem: 'departamento alterado com sucesso'
               })
    
             })
@@ -85,9 +113,23 @@ router.patch('/', (req, res, next) => {
 });
 
 router.delete('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'usando DELETE na rota de departamentos'
-    });
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({error : error})}   
+        conn.query(
+            'DELETE FROM departamento WHERE id = ?',
+            [req.body.id],
+           
+            (error, resultado, field) =>{
+              conn.release();  
+              if (error) { return res.status(500).send({error : error})} 
+              
+              res.status(202).send({
+               mensagem: 'departamento DELETADO com sucesso'
+              })
+   
+            })
+           
+       })
 });
 
 
